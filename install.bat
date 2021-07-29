4eeef30177ec5b78e8ded9408d7ee596fb0db3e2
Nick Manor <nicholas.manor@hotmail.com>
2021-07-28 20:16:23 -0500

    update install batch file

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
-sc create "vMix Lyrics Service" binPath=%CD%/vMixController.exe
+sc create "vMix Lyrics Service" binPath="%CD%/vMixController.exe" start=auto
 
-netsh firewall add allowedprogram program=%CD%/vMixController.exe
+sc failure "vMix Lyrics Service" actions= restart/60000/restart/60000/""/60000 reset= 86400
+
+@REM netsh advfirewall firewall add rule name="vMix Lyrics Service" protocol=tcp localport=5050  dir=in  action=allow
 
 sc start "vMix Lyrics Service"
